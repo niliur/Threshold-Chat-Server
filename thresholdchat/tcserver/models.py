4b219c859883b3d882bdb522e4b10f9ae563dd3f
Corwin Shanner <corwin.shanner@gmail.com>
2019-11-25 23:37:06 -0800

    whoops, broke the database, fixed now

--- thresholdchat/tcserver/models.py
+++ thresholdchat/tcserver/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(primary_key=True)
-    name = models.CharField
+    username = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
     passHash = models.BinaryField
     passSalt = models.BinaryField
     pubKey = models.BinaryField
@@ -26,7 +26,7 @@ class Chat(models.Model):
 
 class Invite(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.chat.name
@@ -34,7 +34,7 @@ class Invite(models.Model):
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     text = models.TextField
 
--- thresholdchat/thresholdchat/urls.py
+++ thresholdchat/thresholdchat/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from tcserver import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tcserver/', include('tcserver.urls')),
 ]
